a3d2701b57e1c6cd6091d5f3d16dd2b3709e0562
fulcrum44 <joelchicano70@gmail.com>
2025-11-05 14:42:58 +0100

    update

--- Android Studio Projects/app/build.gradle.kts
+++ Android Studio Projects/app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
