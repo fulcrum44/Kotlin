ac83c08456b3643d280b3828875389a28827e457
fulcrum44 <joelchicano70@gmail.com>
2025-11-13 14:21:23 +0100

    Navegando por pantallas

---
+++ Navegacion/app/src/main/java/org/izv/jcl/navegacion/MainActivity.kt
@@ -0,0 +1,27 @@
+package org.izv.jcl.navegacion
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.izv.jcl.navegacion.ui.MainScreen
+import org.izv.jcl.navegacion.ui.theme.NavegacionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavegacionTheme {
+                MainScreen()
+            }
+        }
+    }
+}
---
+++ Navegacion/app/src/main/java/org/izv/jcl/navegacion/data/Contact.kt
@@ -0,0 +1,6 @@
+package org.izv.jcl.navegacion.data
+
+data class Contact(
+    val name: String,
+    val phone: String
+)
